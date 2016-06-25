e20d502d56a877875fbad5bdd928948bf325e02a
doktordirk <doktordirk@outlook.de>
2016-06-25 12:09:26 +0200

    feat(configMananger): allow adding instances and added copy, extend and reset.

--- src/configManager.js
+++ src/configManager.js
@@ -1,5 +1,6 @@
 import * as LogManager from 'aurelia-logging';
-import {extendIn} from './baseConfig';
+import extend from 'extend';
+import {BaseConfig} from './baseConfig';
 import {GlobalConfig} from './globalConfig';
 
 /**
@@ -66,7 +67,7 @@ export class ConfigManager {
   }
 
   /**
-   * Load a plugin by moduleId annd merge config
+   * Load a plugin by moduleId and merge config
    * @param  {{moduleId, registerAlias, config, className}} moduleId: string, registerAlias: boolean, config: configObject, className: string
    * @return {Promise<>}
    */
@@ -82,33 +83,123 @@ export class ConfigManager {
 
   /**
    * configure a Config class
-   * @param  {function|string} key           The used DI key for the Config
-   * @param  {Object}          config        The configuration object
-   * @param  {boolen}          registerAlias Register with container (true)
-   * @return this
+   * @param  {function|string|BaseConfig} keyOrInstance The target DI key or an instance of BaseConfig
+   * @param  {Object}                    config        The configuration object
+   * @param  {boolen}                    registerAlias Register with container (true)
+   * @return {ConfigManager}                           itself
    * @chainable
    */
-  add(key, config = {}, registerAlias = true) {
-    let currentConfig = this.container.get(key);
-
-    // set default and extend
-    currentConfig.reset();
-    extendIn(currentConfig, 'current', config);
+  add(keyOrInstance, configObject = {}, registerAlias = true) {
+    let config = keyOrInstance instanceof BaseConfig
+                ? keyOrInstance
+                : this.container.get(keyOrInstance);
 
     // get id
-    let id = currentConfig.alias;
-    if (!id || this.configs[id]) {
-      throw new Error(`Config class alias '${id}' is not a valid unique alias.`);
-    }
+    let id = assertId(config.alias, this.configs, 'unique');
 
-    // register currentConfig
-    this.configs[id] = currentConfig;
+    // set default and extend
+    config.reset();
+    extend(true, config.current, configObject);
+
+    // register config
+    this.configs[id] = config;
     if (registerAlias) {
-      this.container.registerAlias(key, id);
+      if (keyOrInstance instanceof BaseConfig) {
+        this.container.registerInstance(keyOrInstance, id);
+      } else {
+        this.container.registerAlias(keyOrInstance, id);
+      }
     }
 
     LogManager.getLogger('aurelia-config').info(`Registered and configured config '${id}'.`);
 
     return this;
   }
+
+  /**
+  * Deep copy a Config into another
+  * @param  {function|string|BaseConfig} targetKeyOrInstance The target DI key or an instance of BaseConfig
+  * @param  {function|string|BaseConfig} sourceKeyOrInstance The source DI key or an instance of BaseConfig
+  * @param  {boolen}                    includeDefaults     Also copy defaults
+  * @return {ConfigManager}                                 itself
+  * @chainable
+  */
+
+  copy(targetKeyOrInstance, sourceKeyOrInstance, includeDefaults) {
+    let targetConfig = targetKeyOrInstance instanceof BaseConfig
+                    ? targetKeyOrInstance
+                    : this.container.get(targetKeyOrInstance);
+    let sourceConfig = sourceKeyOrInstance instanceof BaseConfig
+                    ? sourceKeyOrInstance
+                    : this.container.get(sourceKeyOrInstance);
+
+
+    // set default and extend
+    targetConfig.current = JSON.parse(JSON.stringify(sourceConfig.current));
+    if (includeDefaults) {
+      targetConfig.defaults = JSON.parse(JSON.stringify(sourceConfig.defaults));
+    }
+
+    return this;
+  }
+
+  /**
+  * Deep extend a Config with another
+  * @param  {function|string|BaseConfig} targetKeyOrInstance The target DI key or an instance of BaseConfig
+  * @param  {function|string|BaseConfig} sourceKeyOrInstance The source DI key or an instance of BaseConfig
+  * @param  {boolen}                    includeDefaults     Also merge defaults
+  * @return {ConfigManager}                                 itself
+  * @chainable
+  */
+  extend(targetKeyOrInstance, sourceKeyOrInstance, includeDefaults) {
+    let targetConfig = targetKeyOrInstance instanceof BaseConfig
+                    ? targetKeyOrInstance
+                    : this.container.get(targetKeyOrInstance);
+    let sourceConfig = sourceKeyOrInstance instanceof BaseConfig
+                    ? sourceKeyOrInstance
+                    : this.container.get(sourceKeyOrInstance);
+
+    // set default and extend
+    extend(true, targetConfig.current, sourceConfig.current);
+    if (includeDefaults) {
+      extend(true, targetConfig.defaults, sourceConfig.defaults);
+    }
+
+    return this;
+  }
+
+  /**
+  * Remove a Config from the ConfigManager
+  * @param  {function|string|BaseConfig} keyOrInstance The target DI key or an instance of BaseConfig
+  * @param  {boolen}                    removeAlias   Also remove DI alias (true)
+  * @return {ConfigManager}                           itself
+  * @chainable
+  */
+  remove(keyOrInstance, removeAlias = true) {
+    let config = keyOrInstance instanceof BaseConfig
+                ? keyOrInstance
+                : this.container.get(keyOrInstance);
+
+    // get id
+    let id = assertId(config.alias, this.configs, 'exists');
+
+    // remove config
+    delete this.configs[id];
+    if (removeAlias) {
+      this.container.unregister(id);
+    }
+
+    LogManager.getLogger('aurelia-config').info(`Removed config '${id}'.`);
+
+    return this;
+  }
+}
+
+function assertId(id, map, option) {
+  if (!id || (map && option === 'exists' && !map[id])) {
+    throw new Error(`Config class alias '${id}' is not a valid alias.`);
+  } else if (map && option === 'unique' && map[id]) {
+    throw new Error(`Config class alias '${id}' is not a unique alias.`);
+  }
+  return id;
 }
